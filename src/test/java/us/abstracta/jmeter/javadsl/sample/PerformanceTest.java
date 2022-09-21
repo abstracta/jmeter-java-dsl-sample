a8402ded498875a5445701803f32cbf981a502b8
rabelenda <rabelenda@gmail.com>
2022-09-21 13:48:55 -0300

    Add uberjar configuration as an example for packaging testplan

--- src/test/java/us/abstracta/jmeter/javadsl/sample/PerformanceTest.java
+++ src/test/java/us/abstracta/jmeter/javadsl/sample/PerformanceTest.java
@@ -4,8 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.Duration;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import us.abstracta.jmeter.javadsl.core.TestPlanStats;
 
 public class PerformanceTest {
@@ -18,6 +24,18 @@ public class PerformanceTest {
         )
     ).run();
     assertThat(stats.overall().sampleTimePercentile99()).isLessThan(Duration.ofSeconds(5));
+  }
+
+  public static void main(String[] args) {
+    SummaryGeneratingListener summaryListener = new SummaryGeneratingListener();
+    LauncherFactory.create()
+        .execute(LauncherDiscoveryRequestBuilder.request()
+                .selectors(DiscoverySelectors.selectClass(PerformanceTest.class))
+                .build(),
+            summaryListener);
+    TestExecutionSummary summary = summaryListener.getSummary();
+    summary.printFailuresTo(new PrintWriter(System.err));
+    System.exit(summary.getTotalFailureCount() > 0 ? 1 : 0);
   }
 
 }
